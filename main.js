b10f53da1cbb518a5de0ef82a46eb8d70d0b498e
habibiahmada <habibiahmadaziz@gmail.com>
2024-01-08 18:12:52 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log("apa liat liat console!! pengen nyontek kan lu?!!");
+
+const base_api = "https://api.genderize.io";
+
+function showResult(name, gender, probability) {
+  const predictionElement = document.getElementById("prediction");
+  const probabilityPercentage = probability * 100;
+  let genderDecode;
+  if (gender == "male") {
+    genderDecode = "Cowok";
+  } else {
+    genderDecode = "Cewek";
+  }
+  const predictionText = `Halo ${name} Jenis Kelamin Kamu Kemungkinan Adalah ${genderDecode} sebesar ${probabilityPercentage}%`;
+
+  predictionElement.textContent = predictionText;
+}
+
+async function predict(event) {
+  if (event.key == "Enter") {
+    const firstName = event.target.value;
+    const queryUrl = `${base_api}/?name=${firstName}&country_id=ID`;
+
+    const response = await fetch(queryUrl);
+    const result = await response.json();
+    showResult(result.name, result.gender, result.probability);
+  }
+}
