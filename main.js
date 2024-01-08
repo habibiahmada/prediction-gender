4b64884e434359ec858f1c8973ec336e34e901e5
habibiahmada <habibiahmadaziz@gmail.com>
2024-01-08 21:12:15 +0700

    second commit

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function showResult(name, gender, probability) {
 async function predict(event) {
   if (event.key == "Enter") {
     const firstName = event.target.value;
-    const queryUrl = `${base_api}/?name=${firstName}&country_id=ID`;
+    const queryUrl = `${base_api}/?name=${firstName}`;
 
     const response = await fetch(queryUrl);
     const result = await response.json();
